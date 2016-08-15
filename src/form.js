fd9daee7ec79f35529faa2a41944b8b0dc04102d
ProkhorovStanislav <karser2009@mail.ru>
2016-08-15 23:04:37 +0300

    form validation

--- src/form.js
+++ src/form.js
@@ -3,6 +3,13 @@
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
+  var formMain =  document.querySelector('.review-form');
+  var userName = formMain.querySelector('.review-form-field-name');
+  var reviewText = formMain.querySelector('.review-form-field-text');
+  var hintsFields = formMain.querySelector('.review-fields');
+  var hintName = hintsFields.querySelector('.review-fields-name');
+  var hintDescription = hintsFields.querySelector('.review-fields-text');
+  var submitBtn = formMain.querySelector('.review-submit');
 
   var form = {
     onClose: null,
@@ -24,11 +31,50 @@ window.form = (function() {
     }
   };
 
+  userName.required = true;
+  submitBtn.disabled = true;
+  userName.oninput = checkRequirements;
+  reviewText.oninput = checkRequirements;
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     form.close();
   };
+
+  function checkRequirements() {
+    var COMPARED_NUMBER = 3;
+
+    var selectedMark = formMain.querySelector('input[type=\'radio\']:checked').value;
+
+    reviewText.required = selectedMark < COMPARED_NUMBER;
+
+    if (isEmptyField(userName)) {
+      hintName.classList.remove('invisible');
+    } else {
+      hintName.classList.add('invisible');
+    }
+
+    if (isEmptyField(reviewText)) {
+      hintDescription.classList.remove('invisible');
+    } else {
+      hintDescription.classList.add('invisible');
+    }
+
+    if (!isEmptyField(userName) && !isEmptyField(reviewText)) {
+      hintsFields.classList.add('invisible');
+    } else {
+      hintsFields.classList.remove('invisible');
+    }
+
+    submitBtn.disabled = isEmptyField(userName) || isEmptyField(reviewText);
+  }
+
+  function isEmptyField(input) {
+    if (input.value) {
+      return false;
+    }
+    return true;
+  }
 
   return form;
 })();
